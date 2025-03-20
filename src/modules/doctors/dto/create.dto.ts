90cf3f886fa5d4cc6312e78fe3a8fd8e4d1177c4
Somto <=>
2025-03-20 20:29:46 +0100

    feat: slot creation schema

--- src/modules/doctors/dto/create.dto.ts
+++ src/modules/doctors/dto/create.dto.ts
@@ -7,4 +7,32 @@ export const createDoctorSchema = z.object({
   lastName: nonEmptyString,
 });
 
+export const createSlotSchema = z
+  .object({
+    startTime: z.date({coerce: true}).min(new Date()),
+    endTime: z.date({coerce: true}),
+    duration: z.literal(15).or(z.literal(30)),
+    daily: z.boolean().default(false),
+    weekdays: z
+      .enum([
+        'MONDAY',
+        'TUESDAY',
+        'WEDNESDAY',
+        'THURSDAY',
+        'FRIDAY',
+        'SATURDAY',
+        'SUNDAY',
+        'EVERYDAY',
+      ])
+      .array()
+      .min(1)
+      .optional(),
+  })
+  .refine(({daily, weekdays}) => !(daily && weekdays), {
+    message: 'Cannot add both daily and weekdays',
+    path: ['weekdays'],
+  });
+
 export type CreateDoctorDto = z.infer<typeof createDoctorSchema>;
+
+export type CreateSlotDto = z.infer<typeof createSlotSchema>;
